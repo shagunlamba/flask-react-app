cf6fd45608b6c85c9c714ef52b2934f4bcab16e4
shagunlamba <lamba.shagun0@gmail.com>
2021-06-17 23:42:47 +0530

    install bootstrap, routing, icons modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,18 @@
 import React, {useState, useEffect} from 'react';
 
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
 
-  const [currentTime, setCurrentTime] = useState(0);
+  const [value, setValue] = useState('');
 
   useEffect(()=>{
-    fetch('/time').then((res)=>res.json()).then((data)=>{
-      setCurrentTime(data.name)
+    fetch('/dummyapi').then((res)=>res.json()).then((data)=>{
+      setValue(data.name)
     })
   },[])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}</p>
-      </header>
+    <div>
+        <h1>The data fetched is {value}</h1>
     </div>
   );
 }
--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/time")
+@app.route("/dummyapi")
 def hello():
     return {
         'name' : "Hello World"
